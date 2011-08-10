29cdfa6dcdf1a62646354b28f075513c3754b0aa
Travis Herrick <tthetoad@gmail.com>
2011-08-09 20:27:53 -0400

    Corrected requirement path for main code in test require file.

--- test/require.rb
+++ test/require.rb
@@ -2,7 +2,7 @@ Bundler.require :test
 
 require 'test/unit'
 
-require_relative '../lib/app_mode/app_mode'
+require_relative '../lib/app_mode'
 
 require_relative 'lib/app_mode_support'
 require_relative 'lib/app_mode'
